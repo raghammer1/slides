99039596ba3e5278bc15560288f0d9ac1338a4b1
Raghav Agarwal <raghagarwal@gmail.com>
2024-04-05 17:46:01 +1100

    Can now drap and resize textbox and image

--- frontend/src/PresentationPage/Slides/SlideDisplay.js
+++ frontend/src/PresentationPage/Slides/SlideDisplay.js
@@ -123,25 +123,45 @@ const SlideDisplay = ({ presentationId, selectedSlideId }) => {
       {selectedSlide?.elements?.map((element) => {
         if (element.type === 'textarea') {
           return (
-            <div key={element.id}>
-              <textarea
-                defaultValue={element.text}
-                style={{
-                  position: 'absolute',
-                  top: element.top,
-                  left: element.left,
-                  height: `${element.height}%`,
-                  width: `${element.width}%`,
-                  fontSize: element.fontSize,
-                  color: element.color,
-                  resize: 'none',
-                  overflow: 'hidden',
-                  border: '2px solid #999',
-                }}
-                onClick={() => handleSelectedElement(element)}
-              />
-              {renderCornerBoxes(element)}
-            </div>
+            <Rnd
+              default={{
+                x: element.top,
+                y: element.left,
+                width: `${element.width}%`, // Initial width based on element's width
+                height: `${element.height}%`, // Initial height based on element's height
+              }}
+              className={element.id}
+              // minWidth={(parseFloat(element.width) / 100) * 1000}
+              // minHeight={(parseFloat(element.height) / 100) * 500}
+              bounds="parent"
+              key={element.id}
+              onDragStop={(e, d) => onDragStop(e, d, element)}
+              onResizeStop={(e, direction, ref, delta, position) =>
+                onResizeStop(e, direction, ref, delta, position, element)
+              }
+            >
+              <div key={element.id}>
+                <textarea
+                  defaultValue={element.text}
+                  style={{
+                    position: 'absolute',
+                    top: element.top,
+                    left: element.left,
+                    height: '100%',
+                    width: '100%',
+                    fontSize: element.fontSize,
+                    color: element.color,
+                    resize: 'none',
+                    overflow: 'hidden',
+                    border: '2px solid #999',
+                    cursor: 'default',
+                  }}
+                  readOnly
+                  onClick={() => handleSelectedElement(element)}
+                />
+                {renderCornerBoxes(element)}
+              </div>
+            </Rnd>
           );
         } else if (element.type === 'image') {
           return (
