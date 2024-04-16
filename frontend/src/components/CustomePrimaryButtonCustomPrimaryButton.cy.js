31f5c056b81ebbed8354a8edd497c189df2a007f
Vansh Patel <z5406654@ad.unsw.edu.au>
2024-04-16 17:17:18 +1000

    testigcomponents

---
+++ frontend/src/components/CustomePrimaryButtonCustomPrimaryButton.cy.js
@@ -0,0 +1,9 @@
+// import React from 'react'
+// import CustomPrimaryButton from './CustomePrimaryButton'
+
+// describe('<CustomPrimaryButton />', () => {
+//   it('renders', () => {
+//     // see: https://on.cypress.io/mounting-react
+//     cy.mount(<CustomPrimaryButton />)
+//   })
+// })
