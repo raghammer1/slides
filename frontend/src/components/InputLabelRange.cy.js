4352fdf6e5589a3d30bd22dbbc6df1355d340294
Vansh Patel <z5406654@ad.unsw.edu.au>
2024-04-17 00:08:53 +1000

    Another test file added for the component part

---
+++ frontend/src/components/InputLabelRange.cy.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount } from '@cypress/react'; // Import mount from @cypress/react
+import InputLabelRange from './InputLabelRange';
+
+describe('<InputLabelRange />', () => {
+  it('renders', () => {
+    // Mount the InputLabelRange component
+    mount(<InputLabelRange />);
+    // Assertion to ensure the component is rendered
+    cy.get('input[type="range"]').should('exist');
+  });
+
+  it('updates the value when slider changes', () => {
+    // Mount the InputLabelRange component
+    mount(<InputLabelRange />);
+
+    // Simulate changing the slider value
+    cy.get('input[type="range"]')
+      .invoke('val', 70)
+      .trigger('change', { force: true });
+
+    // Assertion to ensure the slider value is updated
+    cy.get('input[type="range"]').should('have.value', '70');
+  });
+});
