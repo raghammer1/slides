e8a1d8ff27d60716493432237a6544fb0866e9a9
Raghav Agarwal <raghagarwal@gmail.com>
2024-04-17 23:44:46 +1000

    Starting code cleaning now

--- frontend/src/components/AlertError.js
+++ frontend/src/components/AlertError.js
@@ -1,84 +1,3 @@
-// import React, { createContext, useContext, useState } from 'react';
-// import Snackbar from '@mui/material/Snackbar';
-// import SnackbarContent from '@mui/material/SnackbarContent';
-// import { makeStyles } from '@mui/styles';
-// import CloseIcon from '@mui/icons-material/Close';
-// import { IconButton } from '@mui/material';
-// import { styled } from '@mui/system';
-
-// const AlertContext = createContext();
-
-// // const useStyles = makeStyles((theme) => ({
-// //   snackbar: {
-// //     backgroundColor: (props) => props.bgColor || theme.palette.primary.main,
-// //   },
-// // }));
-// const StyledSnackbarContent = styled(SnackbarContent)(({ theme, bgColor }) => ({
-//   backgroundColor: bgColor || theme.palette.primary.main,
-// }));
-
-// export const useAlert = () => useContext(AlertContext);
-
-// export const AlertProvider = ({ children }) => {
-//   const [alertState, setAlertState] = useState({
-//     open: false,
-//     message: '',
-//     bgColor: 'default',
-//   });
-
-//   const { bgColor } = alertState;
-//   const classes = useStyles({ bgColor });
-
-//   const showAlert = (message, bgColor = 'default') => {
-//     setAlertState({
-//       open: true,
-//       message,
-//       bgColor,
-//     });
-//   };
-
-//   const handleClose = (event, reason) => {
-//     if (reason === 'clickaway') {
-//       return;
-//     }
-//     setAlertState((prevState) => ({
-//       ...prevState,
-//       open: false,
-//     }));
-//   };
-
-//   return (
-//     <AlertContext.Provider value={{ showAlert }}>
-//       {children}
-//       <Snackbar
-//         open={alertState.open}
-//         autoHideDuration={6000}
-//         onClose={handleClose}
-//         anchorOrigin={{
-//           vertical: 'top',
-//           horizontal: 'right',
-//         }}
-//       >
-//         <SnackbarContent
-//           className={classes.snackbar}
-//           style={{ backgroundColor: alertState.bgColor }}
-//           message={<span id="client-snackbar">{alertState.message}</span>}
-//           action={[
-//             <IconButton
-//               key="close"
-//               aria-label="Close"
-//               color="inherit"
-//               onClick={handleClose}
-//             >
-//               <CloseIcon style={{ color: '#fff' }} />
-//             </IconButton>,
-//           ]}
-//         />
-//       </Snackbar>
-//     </AlertContext.Provider>
-//   );
-// };
-
 import React, { createContext, useContext, useState } from 'react';
 import Snackbar from '@mui/material/Snackbar';
 import SnackbarContent from '@mui/material/SnackbarContent';
@@ -130,7 +49,8 @@ export const AlertProvider = ({ children }) => {
         anchorOrigin={{
           vertical: 'top',
           horizontal: 'right',
-        }}>
+        }}
+      >
         <StyledSnackbarContent
           bgColor={alertState.bgColor} // Pass bgColor as a prop to styled component
           message={<span id="client-snackbar">{alertState.message}</span>}
@@ -139,7 +59,8 @@ export const AlertProvider = ({ children }) => {
               key="close"
               aria-label="Close"
               color="inherit"
-              onClick={handleClose}>
+              onClick={handleClose}
+            >
               <CloseIcon style={{ color: '#fff' }} />
             </IconButton>,
           ]}
