cff71b6168196d15b446bd10c97b228c9edbec95
Raghav Agarwal <raghagarwal@gmail.com>
2024-04-18 18:31:58 +1000

    User can now delete all presentations at once

--- frontend/src/dashboard/MainDashboard.js
+++ frontend/src/dashboard/MainDashboard.js
@@ -1,16 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { styled } from '@mui/system';
 import CreatePresentationModal from './CreatePresentationModal';
 import PresentationList from './PresentationList/PresentationList';
-import CustomPrimaryButton from '../components/CustomePrimaryButton';
 import usePresentationListStore from '../zustandStore/usePresentationListStore';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Button from '@mui/material/Button';
-import { Tooltip } from '@mui/material';
 
 // MainDashboard component that orchestrates the presentation of lists and modal forms within the dashboard.
 const MainDashboard = ({ open, handleClose, searchInput }) => {
@@ -23,76 +15,19 @@ const MainDashboard = ({ open, handleClose, searchInput }) => {
     gap: '20px',
   });
 
-  const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
-  const handleOpenConfirmDialog = () => setOpenConfirmDialog(true);
-  const handleCloseConfirmDialog = () => setOpenConfirmDialog(false);
-
-  const clearPresentations = usePresentationListStore(
-    (state) => state.clearPresentations
-  );
   const { presentations } = usePresentationListStore((state) => ({
     presentations: state.presentations,
   }));
 
-  const handleDeleteConfirm = () => {
-    clearPresentations();
-    handleCloseConfirmDialog();
-  };
-
   return (
     <>
       <Wrapper>
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: '20px',
-          }}
-        >
-          <Tooltip
-            title={
-              presentations.length === 0 ? 'No presentation to delete' : ''
-            }
-          >
-            <div>
-              <CustomPrimaryButton
-                label={'Delete All Presentations'}
-                disabled={presentations.length === 0}
-                additionalStyle={{
-                  width: '250px',
-                  height: '35px',
-                }}
-                onClick={handleOpenConfirmDialog}
-              />
-            </div>
-          </Tooltip>
-        </div>
         <PresentationList
           presentations={presentations}
           searchInput={searchInput}
         />
         <CreatePresentationModal open={open} handleClose={handleClose} />
       </Wrapper>
-      <Dialog
-        open={openConfirmDialog}
-        onClose={handleCloseConfirmDialog}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">{'Confirm Deletion'}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Are you sure you want to delete all presentations?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseConfirmDialog}>Cancel</Button>
-          <Button onClick={handleDeleteConfirm} autoFocus>
-            Yes, Delete
-          </Button>
-        </DialogActions>
-      </Dialog>
     </>
   );
 };
--- frontend/src/dashboard/PresentationList/PresentationList.js
+++ frontend/src/dashboard/PresentationList/PresentationList.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PresentationCard from './PresentationCard';
 import styled from '@emotion/styled';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Button from '@mui/material/Button';
+import { Tooltip } from '@mui/material';
+import usePresentationListStore from '../../zustandStore/usePresentationListStore';
+import CustomPrimaryButton from '../../components/CustomePrimaryButton';
 
 // Wrapper for presentation list that applies a flex layout.
 const Wrapper = styled.div`
@@ -10,10 +19,7 @@ const Wrapper = styled.div`
   gap: 20px;
   flex-wrap: wrap;
   overflow: auto;
-  padding: 20px;
-  align-items: flex-start;
   justify-content: center;
-  align-content: flex-start;
 `;
 
 // PresentationList component filters and displays a list of presentation cards.
@@ -22,13 +28,73 @@ const PresentationList = ({ presentations, searchInput }) => {
   const filteredPresentations = presentations.filter((presentation) =>
     presentation.name.toLowerCase().includes(searchInput.toLowerCase())
   );
+  const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
+  const handleOpenConfirmDialog = () => setOpenConfirmDialog(true);
+  const handleCloseConfirmDialog = () => setOpenConfirmDialog(false);
 
+  const handleDeleteConfirm = () => {
+    clearPresentations();
+    handleCloseConfirmDialog();
+  };
+
+  const clearPresentations = usePresentationListStore(
+    (state) => state.clearPresentations
+  );
   return (
-    <Wrapper>
-      {filteredPresentations.map((presentation) => (
-        <PresentationCard key={presentation.id} presentation={presentation} />
-      ))}
-    </Wrapper>
+    <div
+      style={{
+        justifyContent: 'center',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '20px',
+        }}
+      >
+        <Tooltip
+          title={presentations.length === 0 ? 'No presentation to delete' : ''}
+        >
+          <div>
+            <CustomPrimaryButton
+              label={'Delete All Presentations'}
+              disabled={presentations.length === 0}
+              additionalStyle={{
+                width: '250px',
+                height: '35px',
+              }}
+              onClick={handleOpenConfirmDialog}
+            />
+          </div>
+        </Tooltip>
+      </div>
+      <Wrapper>
+        {filteredPresentations.map((presentation) => (
+          <PresentationCard key={presentation.id} presentation={presentation} />
+        ))}
+      </Wrapper>
+      <Dialog
+        open={openConfirmDialog}
+        onClose={handleCloseConfirmDialog}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{'Confirm Deletion'}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete all presentations?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseConfirmDialog}>Cancel</Button>
+          <Button onClick={handleDeleteConfirm} autoFocus>
+            Yes, Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
