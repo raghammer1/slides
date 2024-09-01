bfcfc6190235109be0262d29af2b696cc49e2ef4
raghammer1 <raghagarwal@gmail.com>
2024-09-01 20:45:54 +1000

    hh

--- backend/src/server.js
+++ backend/src/server.js
@@ -34,7 +34,7 @@ const catchErrors = (fn) => async (req, res) => {
       res.status(403).send({ error: err.message });
     } else {
       console.log(err);
-      res.status(500).send({ error: 'A system error occurred' });
+      res.status(500).send({ error: 'A system error ocurred' });
     }
   }
 };
@@ -112,19 +112,9 @@ const configData = JSON.parse(
 );
 const port = 'BACKEND_PORT' in configData ? configData.BACKEND_PORT : 5000;
 
-    const server = app.listen(port, () => {
-      console.log(`Backend is now listening on port ${port}!`);
-      console.log(`For API docs, navigate to http://localhost:${port}`);
-    });
-
-    serverStarted = true; // Mark server as started
-    return server;
-  } else {
-    console.log('Server is already running. Cannot start it again.');
-    return null;
-  }
-};
-
-const server = startServer(); // Start the server
+const server = app.listen(port, () => {
+  console.log(`Backend is now listening on port ${port}!`);
+  console.log(`For API docs, navigate to http://localhost:${port}`);
+});
 
 export default server;
